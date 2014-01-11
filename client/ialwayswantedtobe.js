7946ccf9af215559284fa0c463dedf6594db1504
JakeKahana <jakekahana@gmail.com>
2014-01-11 12:24:55 -0600

    Update preload and hide buttons

--- client/ialwayswantedtobe.js
+++ client/ialwayswantedtobe.js
@@ -1,118 +1,142 @@
 if (Meteor.isClient) {
-  // on startup, load 10 jobs
-  // then track when someone scrolls through 5 and fetch 10 more from the server
-  // we're storing the loaded jobs in an array and just moving through it
-
-  // to load 10 jobs would be something like
-  // Meteor.call('loadNextTenJobs', function(err, result){
-  //  put the resulting 10 jobs into our temporary array
-  // });
+    // on startup, load 10 jobs
+    // we're storing the loaded jobs in an array and just moving through it
+    // to load 10 jobs would be something like
+    // Meteor.call('loadNextTenJobs', function(err, result){
+    //  put the resulting 10 jobs into our temporary array
+    // });
 }
-
 Meteor.subscribe("demodata");
 console.log(Demodata.find({}));
 
-var changebackground = function(){  
-    (function() {
-                var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-                $.getJSON( flickerAPI, {
-                  tags: document.getElementById("job").innerHTML,
-                  tagmode: "all",
-                  format: "json"
-              })
-            .done(function( data ) {
-              $.each( data.items, function( i, item ) {
-                if(!item){
-                  document.getElementById("bgimg").src = "http://fc09.deviantart.net/fs71/i/2010/346/0/0/dream_by_taenaron-d34q599.jpg";
-                }
-                else{
-                    var sourceLarge = (item.media.m).replace("_m.jpg", "_b.jpg");
-                    //$( "<img>" ).attr( "src", sourceLarge ).appendTo( "#background" );
-                    document.getElementById("bgimg").src = sourceLarge;
-                    if ( i === 0 ) {
-                    return false;
-                    }
+alljobs[1].bg.push("elephant");
 
-                  }
-       
-              });
-            });
-    })();
+var preload = function() {
+    for (var j = 0; j < alljobs.length; j++) {
+        var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        $.getJSON(flickerAPI, {
+            tags: alljobs[j].title,
+            tagmode: "all",
+            format: "json"
+        }).done(function(data) {
+            $.each(data.items, function(i, item) {
+                var sourceLarge = (item.media.m).replace("_m.jpg", "_b.jpg");
+                alljobs.replace(alljobs[j].bg, sourceLarge);
+                if (i === 0) {
+                    return false;
+                }
+            })
+        });
+    }
+}
+
+var changebackground = function(counter) {
+    // var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    // $.getJSON(flickerAPI, {
+    //     //tags: document.getElementById("job").innerHTML,
+    //     tags: alljobs[counter].title,
+    //     tagmode: "all",
+    //     format: "json"
+    // })
+    //     .done(function(data) {
+    //         $.each(data.items, function(i, item) {
+    //             var sourceLarge = (item.media.m).replace("_m.jpg", "_b.jpg");
+    //             //$( "<img>" ).attr( "src", sourceLarge ).appendTo( "#background" );
+    //             document.getElementById("bgimg").src = allbkgrds[counter];
+    //             if (i === 0) {
+    //                 return false;
+    //             }
+    //         })
+    //     });
+    document.getElementById("bgimg").src = alljobs[counter].bg;
+
 }
 
 var counter = 2;
 
 
 Template.slideshow.events({
-  'click #nextslide': function(){
-    var tempjob = document.getElementById('job');
-    var tempdesc = document.getElementById('description');
-    if (counter <= alljobs.length) {
-      counter += 1;
-      tempjob.innerHTML = alljobs[counter].title;
-      tempdesc.innerHTML = alljobs[counter].description; 
-      changebackground();
-   
+    'click #nextslide': function() {
+        var tempjob = document.getElementById('job');
+        var tempdesc = document.getElementById('description');
+        counter += 1;
+        if (counter < alljobs.length) {
+            tempjob.innerHTML = alljobs[counter].title;
+            tempdesc.innerHTML = alljobs[counter].description;
+            changebackground(counter);
+            console.log(counter)
+            document.getElementById('previousslide').style.display = "block";
+        } else {
+            document.getElementById('nextslide').style.display = "none";
+            console.log(counter)
+        }
+        // if (counter = alljobs.length - 5) {
+        //load 10 new items in tempdata
+        // }
     }
-  }
 })
 
 Template.slideshow.events({
-  'click #previousslide': function(){
-    var tempjob = document.getElementById('job');
-    var tempdesc = document.getElementById('description');
-    if (counter > 0) {
-      counter -= 1;
-      tempjob.innerHTML = alljobs[counter].title;
-      tempdesc.innerHTML = alljobs[counter].description;
-      changebackground();
-    };
-  }
+    'click #previousslide': function() {
+        var tempjob = document.getElementById('job');
+        var tempdesc = document.getElementById('description');
+        counter -= 1;
+        if (counter >= 0) {
+            tempjob.innerHTML = alljobs[counter].title;
+            tempdesc.innerHTML = alljobs[counter].description;
+            changebackground(counter);
+            document.getElementById('nextslide').style.display = "block";
+        } else {
+            document.getElementById('previousslide').style.display = "none";
+        }
+    }
 })
 
 Template.slideshow.events({
-  'change #job': function(){
-    console.log("abc");
-    changebackground();
-  }
+    'change #job': function() {
+        console.log("abc");
+        changebackground();
+    }
 });
 
 Template.form.events({
-  'click #submit': function(){
-    alljobs.push({
-      title: document.getElementById("formword").value,
-      description: document.getElementById("formdescription").value
-    });
-    $('#form').fadeOut(350, function(){
-        $("#slideshow").fadeIn(750);
-    });
-  }
+    'click #submit': function() {
+        alljobs.push({
+            title: document.getElementById("formword").value,
+            description: document.getElementById("formdescription").value
+        });
+        $('#form').fadeOut(350, function() {
+            $("#slideshow").fadeIn(750);
+        });
+        preload();
+    }
 })
 
 Template.form.events({
-  'click #skip': function(){
-  //make sure it's not empty data
-    document.getElementById("job").innerHTML = alljobs[2].title;
-    document.getElementById("description").innerHTML = alljobs[2].description;  //add to collection
+    'click #skip': function() {
+        //make sure it's not empty data
+        document.getElementById("job").innerHTML = alljobs[2].title;
+        document.getElementById("description").innerHTML = alljobs[2].description; //add to collection
 
-    $('#form').fadeOut(350, function(){
-        $("#slideshow").fadeIn(750);
-      });
-  }
+        $('#form').fadeOut(350, function() {
+            $("#slideshow").fadeIn(750);
+        });
+        preload();
+    }
 })
 
 Template.footer.events({
-//show about popup if you click "about"
-  'click #abouttype': function(){
-    $('#aboutpopup').slideToggle(300);
-    document.getElementById("aboutpopup").style.display = "block";
-  }
+    //show about popup if you click "about"
+    'click #abouttype': function() {
+        $('#aboutpopup').slideToggle(300);
+        document.getElementById("aboutpopup").style.display = "block";
+    }
 });
 //hide about popup if your mouse leaves the popup
 Template.footer.events({
-  'mouseleave #aboutpopup': function(){
-    $('#aboutpopup').slideToggle(400);
-  }
+    'mouseleave #aboutpopup': function() {
+        $('#aboutpopup').slideToggle(400);
+    }
 });
 
 //hide "about" popup if you hit ESCAPE
--- client/tempdata.js
+++ client/tempdata.js
@@ -1,23 +1,31 @@
-alljobs = [
-	{	"title": "Doctor",
-		"description": "My mom was a doctor"
-	},
- 	{	"title": "Barbie",
-		"description": "Ken. Obviously."
-	},
-	{	"title": "Beyoncé",
-		"description": "She's the queen."
-	},
-	{	"title": "Bill Gates",
-		"description": "He cured malaria. Or polio."
-	},
-	{	"title": "Bum",
-		"description": "Just Testing out the site."
-	},
-	{	"title": "Martha Stewart",
-		"description": "yaaaaay."
-	},
-	{	"title": "ehlsdkfjsbdkjfg",
-		"description": "I just threw up."
-	}
-	]
+alljobs = [{
+    "title": "Doctor",
+    "description": "My mom was a doctor",
+    "bg": ""
+}, {
+    "title": "Barbie",
+    "description": "Ken. Obviously.",
+    "bg": ""
+}, {
+    "title": "Beyoncé",
+    "description": "She's the queen.",
+    "bg": ""
+}, {
+    "title": "Bill Gates",
+    "description": "He cured malaria. Or polio.",
+    "bg": ""
+}, {
+    "title": "Bum",
+    "description": "Just Testing out the site.",
+    "bg": ""
+}, {
+    "title": "Martha Stewart",
+    "description": "yaaaaay.",
+    "bg": ""
+}, {
+    "title": "ehlsdkfjsbdkjfg",
+    "description": "I just threw up.",
+    "bg": ""
+}]
+
+allbkgrds = [];
